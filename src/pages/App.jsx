0013fbe3038430ac9a36d9fab755e4d995dd3bab
Kieran <kebusby@hotmail.co.uk>
2024-01-09 22:48:47 +0000

    Background dynamic and formatted correctly using CDN

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import React, { useState, useEffect, useRef } from 'react'
 import Styles from '../App.module.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -11,27 +11,33 @@ import TOPOLOGY from 'vanta/dist/vanta.topology.min'
 
 export default function App() {
   // Do i need to change the background color here? Is there enough contrast?
+  const [vantaEffect, setVantaEffect] = useState(null)
+  const myRef = useRef(null)
   useEffect(() => {
-    TOPOLOGY({
-      el: "#bg",
-  mouseControls: true,
+    if (!vantaEffect) {
+      setVantaEffect(TOPOLOGY({
+        el: myRef.current,
+        color: 0x0,
+        backgroundColor: 0xffffff,
+        mouseControls: true,
   touchControls: true,
   gyroControls: false,
   minHeight: 200.00,
   minWidth: 200.00,
   scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0x0,
-  backgroundColor: 0xffffff
-    })
-  }, [])
+  scaleMobile: 1.00
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
 
   return (
     // Div name set to ensure the background is applied to the entire page
     <>
-      <div id="bg" className={Styles.vanta}>
-      
-        <main >
+      <div className={Styles.vanta} ref={myRef}>
+<main className={Styles.vanta}  >
           <Header />
           <section className={Styles.mainContainer}>
             <div className='divider'> </div>
@@ -46,9 +52,11 @@ export default function App() {
             <Socials />
           </section>
         </main>
+
+      </div>
+        
         
 
-        </div>
       
       
       
