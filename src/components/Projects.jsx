3658ff99b5a8068dc67189c3e585badc36ed05e5
Kieran <kebusby@hotmail.co.uk>
2024-01-10 22:08:34 +0000

    Added some Media Queries to the projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,11 @@
 import Styles from '../styles/projects.module.css'
 import TeamRandomiser from '../assets/TeamRandomiser.png'
+import BeyondBarriers from '../assets/BeyondBarriers.png'
 
 export default function Projects() {
     return (
+        <div className={Styles.projectContainer}>
+        {/* Start of Project Number One */}
         <div className={Styles.container}>
             <section className={Styles.title}>
                 <h3>Football Team Randomiser</h3>
@@ -13,9 +16,27 @@ export default function Projects() {
 
             </section>
             <section className={Styles.description}>
-                <p>Try my app <a target='_blank' href='https://safc-team-randomiser.vercel.app'>here</a>.</p>
+                <p>Try the app <a target='_blank' href='https://safc-team-randomiser.vercel.app'>here</a>.</p>
                 <p>See the <a target='_blank' href='https://github.com/KeBubs/TeamRandomiser'>code</a>.</p>
             </section>
+        </div>
+        {/* End of Project Number One  */}
+
+        {/*  Start of Project Number Two  */}
+        <div className={Styles.container}>
+            <section className={Styles.title}>
+                <h3>Disability Community App</h3>
+            </section>
+            <section className={Styles.middleSection}>
+                <img className={Styles.image} src={BeyondBarriers}/>
+                <p>Created for an external staekholder, this Web App allows the user to search for venues by a venue name/type. Once the venues have been returned, they are then able to use the filters provided to find venues which are accessible to their needs. </p>
+
+            </section>
+            <section className={Styles.description}>
+                <p>Try the app <a target='_blank' href='https://beyond-barriers.vercel.app'>here</a>.</p>
+            </section>
+        </div>
+        {/* End of Project Two  */}
         </div>
     )
     }
